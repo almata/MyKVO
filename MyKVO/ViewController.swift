51be19cf82aa748d6c2729f0d7602c9e72b65a30
Albert Mata <hello@albertmata.net>
2018-12-05 11:55:49 +0100

    Initial Commit

---
+++ MyKVO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyKVO
+//
+//  Created by Albert Mata Guerra on 05/12/2018.
+//  Copyright © 2018 Albert Mata Guerra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
